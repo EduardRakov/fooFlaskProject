e1a3322179f4ec159119882b679bc5027ef87942
Eduard Rakov <eduardr@softwareplanet.uk.com>
2016-12-05 21:50:51 +0200

    Add supporting of mimetypes

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,25 +1,32 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
-app = Flask(__name__) #WSGI application
+app = Flask(__name__)  # WSGI application
 
 
 @app.route('/hello')
 def hello():
-    name = request.args.get('name')
-    if not is_param_valid(name):
-        return invalid_param_response()
-
-    return "<h1>Hello, %s</h1>" % name
+    return prepared_response("Hello")
 
 
 @app.route('/bye')
 def bye():
+    return prepared_response("Goodbye")
+
+
+def prepared_response(message):
     name = request.args.get('name')
     if not is_param_valid(name):
         return invalid_param_response()
 
-    return "<h1>Goodbye, %s</h1>" % name
+    result = "{0} {1}".format(message, name)
+
+    if request_wants_json():
+        return jsonify({'message': result})
+
+    if request_wants_text():
+        return result
+
+    return "<h1>{0} {1}</h1>".format(message, name)
 
 
 def is_param_valid(param):
@@ -28,3 +35,13 @@ def is_param_valid(param):
 
 def invalid_param_response():
     return 'Param is empty or invalid', 400
+
+
+def request_wants_json():
+    best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
+    return best == 'application/json' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+
+def request_wants_text():
+    best = request.accept_mimetypes.best_match(['text/plain', 'text/html'])
+    return best == 'text/plain' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
