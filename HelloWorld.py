6b5e5a1294b1c86e250d4284fc28e3751cd7f8cf
Eduard Rakov <eduardr@softwareplanet.uk.com>
2016-12-09 14:43:12 +0200

    Remove extra code

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,25 +1,21 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)  # WSGI application
-
+app = Flask(__name__) # WSGI application
 
 @app.route('/hello')
 def hello():
     return prepared_response("Hello")
 
-
 @app.route('/bye')
 def bye():
     return prepared_response("Goodbye")
 
-
 def prepared_response(message):
     name = request.args.get('name')
-    if not is_param_valid(name):
+    if not name:
         return invalid_param_response()
 
     result = "{0} {1}".format(message, name)
-
     if request_wants_json():
         return jsonify({'message': result})
 
@@ -28,19 +24,12 @@ def prepared_response(message):
 
     return "<h1>{0} {1}</h1>".format(message, name)
 
-
-def is_param_valid(param):
-    return param is not None and param is not ''
-
-
 def invalid_param_response():
     return 'Param is empty or invalid', 400
-
 
 def request_wants_json():
     best = request.accept_mimetypes.best_match(['application/json', 'text/html'])
     return best == 'application/json' and request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
-
 
 def request_wants_text():
     best = request.accept_mimetypes.best_match(['text/plain', 'text/html'])
